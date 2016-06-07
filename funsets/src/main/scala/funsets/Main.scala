26e8a25374eeb19373c77cb73b70d8423f52a0b6
kobykarp <newpasS2>
2016-06-07 16:53:21 +0200

    ex2 submitted

--- funsets/src/main/scala/funsets/Main.scala
+++ funsets/src/main/scala/funsets/Main.scala
@@ -3,4 +3,22 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  var s = union(
+    union(
+      singletonSet(1),
+      singletonSet(2)
+    ),
+    singletonSet(1)
+  )
+  println(forall(s, x => x > 0))
+  println(forall(s, x => x > 1))
+
+  println(exists(s, x => x > 1))
+  println(exists(s, x => x > 2))
+
+  var s2 = map(s, x => x * x)
+  println(exists(s2, x => x == 4))
+
+  printSet(s)
+  printSet(s2)
 }
